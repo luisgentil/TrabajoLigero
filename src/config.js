0c79258a37805b12bcc07f23a527fa03b5913045
luisgentil <luisgentilalperiz@gmail.com>
2024-12-01 16:30:20 +0100

    Cambio BD a Atlas

--- src/config.js
+++ src/config.js
@@ -2,6 +2,6 @@ import { config } from "dotenv";
 config();
 
 export const PORT = process.env.PORT || 4000;
-export const MONGODB_URI = process.env.MONGODB_URI || "mongodb://127.0.0.1/viajes/nombramientos"; //"mongodb://127.0.0.1/notesdb"; //modifico el original localhost
+export const MONGODB_URI = process.env.MONGODB_URI ;//|| "mongodb://127.0.0.1/viajes/nombramientos"; //"mongodb://127.0.0.1/notesdb"; //modifico el original localhost
 
 // moddifico el nombre de la BD >> comienzo usando la de C:, que es viajes/nombramientos
